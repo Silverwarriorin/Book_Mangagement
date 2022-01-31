581de4a7afcd457de4d5ba478b3991d1e61cc0eb
Silverwarriorin <46795935+Silverwarriorin@users.noreply.github.com>
2022-01-31 16:03:12 -0500

    fixed the login, need to style everything else

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,14 @@
 class SessionsController < ApplicationController
-
+  before_action :authenticate_user
   def index
 
   end
+
+  def authenticate_user
+    if session[:userinfo].present?
+      redirect_to '/menu'
+  else
+    redirect_to login_path, notice: "You need to log in"
+    end
+    end
 end

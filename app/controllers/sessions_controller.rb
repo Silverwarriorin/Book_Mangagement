1e02bee9031e1c6d1da4d7dcdc8ca2e7121b6f3c
Silverwarriorin <46795935+Silverwarriorin@users.noreply.github.com>
2022-02-02 13:48:57 -0500

    begun styling everything

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,6 @@ class SessionsController < ApplicationController
   def authenticate_user
     if session[:userinfo].present?
       redirect_to '/menu'
-  else
-    redirect_to login_path, notice: "You need to log in"
-    end
+      end
     end
 end

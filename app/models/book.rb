313243e99fc9e6e59573675a0b27f15745e8bda0
David Bock <dbock@javaguy.org>
2022-02-07 23:26:27 -0500

    adds missing stuff that makes your models useful.
    Too much to explain here. I can show you what this adds in 2 minutes in person or on a screenshare.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,17 @@
 class Book < ApplicationRecord
+  has_many :loans
+  
+  
+  def borrowed?
+    !loans.current.empty?
+  end
+  
+  def borrow(student)
+    Loan.create( book: self, student: student, date_out: Time.now)
+  end
+  
+  def return
+    loans.current[0].return
+  end
+  
 end
